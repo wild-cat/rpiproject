0ea85728dbf1e83ca305932e4020bfd593c090f5
HOLOTA MATEUSZ p1313137 <p1313137@univ-lyon1.fr>
2016-03-08 20:30:53 +0100

    test 2

--- servoLeft.php
+++ servoLeft.php
@@ -9,26 +9,26 @@
  *
  */
 	
-//	for ($i=25;$i>=65;$i+=5) {
+	for ($i=25;$i<=65;$i++) {
 		
 		$cmd = "gpio -1 mode ".$_GET['servoPin']." pwm";
 		system ($cmd);
 		
-		echo "SEND: ".$cmd;
+		echo "SEND: ".$cmd." \r\n";
 		
 		$cmd = "gpio pwm-ms && gpio pwmc 400 && gpio pwmr 1000";
 		system ($cmd);
 		
-		echo "SEND: ".$cmd;
+		echo "SEND: ".$cmd." \r\n";
 		
 		$cmd = "gpio -1 pwm ".$_GET['servoPin']." ".$_GET['spin'];
 		system ($cmd);
 		
-		echo "SEND: ".$cmd;
+		echo "SEND: ".$cmd." \r\n";
 		
 		
-	///	sleep(500);
-//	}
+		sleep(1000);
+	}
 
  	/* 25 is minimum value with these options */
 
