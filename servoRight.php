846758163b3a95e5b6cf97cd6984cb550b4441f5
Mateusz <Mateusz@192.168.1.50>
2016-03-13 15:40:51 +0100

    remove 2nd command

--- servoRight.php
+++ servoRight.php
@@ -9,7 +9,7 @@
  */
 
 	system ("gpio -1 mode ".$_GET['servoPin']." pwm");
-	system ("gpio -1 pwm-ms && gpio pwmc 400 && gpio pwmr 1000");
+	//system ("gpio -1 pwm-ms && gpio pwmc 400 && gpio pwmr 1000");
 	system ("gpio -1 pwm ".$_GET['servoPin']." 105");
 
 	/* 105 is maximum value with these options */
