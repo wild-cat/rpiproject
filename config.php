336fa86f6994f41000cc132a0b39ffddfe773726
Mateusz <Mateusz@192.168.1.50>
2016-03-13 18:25:15 +0100

    init

--- config.php
+++ config.php
@@ -10,10 +10,20 @@
  *
  */
 
+//setup the servos to make them spin
 system ("gpio -1 mode ".$_SESSION['servoPinBasic']." pwm");
 system ("gpio -1 mode ".$_SESSION['servoPinCont']." pwm");
+
+//setup the pin for fire button
 system ("gpio -1 mode ".$_SESSION['firePin']." out");
+
+//init the servos
 system ("gpio pwm-ms && gpio pwmc 400 && gpio pwmr 1000");
+
+//init de basic servo to 'center' position
+system ("gpio -1 pwm ".$_SESSION['servoPinBasic']." 65");
+
+
 
 
 ?>
